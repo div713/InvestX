f4a51d7cae0553764a27f529454ce08adc9352d7
Divyam Khandelwal <divyamkhandelwal215@gmail.com>
2026-06-30 17:02:53 +0530

    changed router settings

--- InvestX/src/landing/Login.jsx
+++ InvestX/src/landing/Login.jsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
+
 function Login() {
-    return ( 
-        <div className="container p-3">
-            <h2 className="mt-3">Login</h2>
-            <form action="/login">
-                <button className="mt-2 btn btn-primary fs-6" style={{width:"20%"}} >Log in to your account</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="container p-3">
+      <h2 className="mt-3">Login</h2>
+      <Link
+        to="/login"
+        className="btn btn-primary mt-2 fs-6"
+        style={{ width: "20%" }}
+      >
+        Log in to your account
+      </Link>
+    </div>
+  );
 }
 
 export default Login;
--- InvestX/src/landing/Openaccount.jsx
+++ InvestX/src/landing/Openaccount.jsx
@@ -1,13 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Openaccount() {
-    return ( 
-        <div className="container p-3">
-            <h2 className="mt-3">Open Account</h2>
-            <p className="mt-2 mb-2">Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-            <form action="/register">
-                <button className="mt-2 btn btn-primary fs-6" style={{width:"20%"}} >Sign Up for free</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="container p-3">
+      <h2 className="mt-3">Open Account</h2>
+      <p className="mt-2 mb-2">
+        Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O
+        trades.
+      </p>
+      <Link
+        to="/register"
+        className="btn btn-primary mt-2 fs-6"
+        style={{ width: "20%" }}
+      >
+        Open your account
+      </Link>
+    </div>
+  );
 }
 
 export default Openaccount;
